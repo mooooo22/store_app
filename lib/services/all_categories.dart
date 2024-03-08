db103146cb905c6797473f2dc422c4053ec278ec
MoAdel <madel7586@gmail.com>
2024-03-08 08:22:28 +0200

    Refactor API error handling in GetAllCategories and GetAllProduct services

--- lib/services/all_categories.dart
+++ lib/services/all_categories.dart
@@ -3,12 +3,21 @@ import 'dart:developer';
 import 'package:dio/dio.dart';
 
 class GetAllCategories {
+  final Dio dio = Dio();
+  final baseUrl = 'https://fakestoreapi.com';
   Future<List<dynamic>> getAllCategories() async {
-    final Dio dio = Dio();
-    final baseUrl = 'https://fakestoreapi.com';
-    Response response = await dio.get('$baseUrl/products/categories');
-    List<dynamic> categories = response.data;
-    log(categories.toString());
-    return categories;
+    try {
+      Response response = await dio.get('$baseUrl/products/categories');
+      List<dynamic> categories = response.data;
+      log(categories.toString());
+      return categories;
+    } on DioException catch (e) {
+
+      final String error =
+          e.response?.data['error']['message'] ?? 'Opps,Something went wrong';
+      throw Exception(error);
+    } catch (e) {
+      throw Exception('Something went wrong');
+    }
   }
 }
--- lib/services/get_category_service.dart
+++ lib/services/get_category_service.dart
@@ -4,20 +4,22 @@ import 'package:store_app/models/product_model.dart';
 class GetAllProduct {
   final Dio dio = Dio();
   final baseUrl = 'https://fakestoreapi.com';
-  Future<List<ProductModel>> getAllProduct() async {
+  Future<List<ProductModel>> getCategory({required String categoryName}) async {
     try {
-      Response response = await dio.get('$baseUrl/products');
+      Response response =
+          await dio.get('$baseUrl/products/category/$categoryName');
       List<ProductModel> products = [];
       response.data.forEach((product) {
         products.add(ProductModel.fromJson(product));
       });
-
       return products;
     } on DioException catch (e) {
+      // TODO
       final String error =
           e.response?.data['error']['message'] ?? 'Opps,Something went wrong';
       throw Exception(error);
-    } catch (e) {
+    }
+    catch (e) {
       throw Exception('Something went wrong');
     }
   }
